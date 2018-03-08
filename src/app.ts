ec9139cf8938c31e19b5e6879d4d7dcbb5889a8e
Satit Rianpit <siteslave@msn.com>
2018-03-08 13:04:31 +0700

    fixed knex configure
    Signed-off-by: Satit Rianpit <siteslave@msn.com>

--- src/app.ts
+++ src/app.ts
@@ -117,7 +117,7 @@ let userAuth = (req, res, next) => {
 
 let dbConnection: MySqlConnectionConfig = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: +process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
