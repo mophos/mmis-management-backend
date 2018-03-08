c4c95eed7178eb321507e711633f0d617a4b5089
Satit Rianpit <siteslave@msn.com>
2018-03-08 12:26:09 +0700

    test travis

--- src/app.ts
+++ src/app.ts
@@ -34,9 +34,6 @@ import  approveRoute  from "./routes/approve";
 
 const app: express.Express = express();
 
-//uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname,'public','favicon.ico')));
-
 //view engine setup
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
